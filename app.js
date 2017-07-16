6b6b4c5293a1355c95801ea407da17593cf28cb0
Maegan Womble <maeganwomble@gmail.com>
2017-07-15 20:02:00 -0500

    added self-updating feature to DOM option elements

--- app.js
+++ app.js
@@ -1,18 +1,46 @@
 $(document).ready(function () {
     $.getJSON('data.json', function (dataObj) {
-
         var wells = dataObj.result.records;
         var selectedWell;
-
-        function findWell(well) {
-            return well.well_name === selectedWell.toUpperCase();
+        
+        //Create an array based on chosen properties
+        function pluckData(array, property){
+            var newArr = array.map(function(obj){
+                return obj[property];
+            });
+            return(newArr);
         }
-
+        
+        //Create an array of well names
+        var wellNames = pluckData(wells, 'well_name');
+        console.log(wellNames);
+        
+        //Update html options from the chosen array
+        function updateOptions (array){
+            var optionView;
+            optionView += '<option>SELECT WELL</option>';
+            array.forEach(function(item){
+                optionView += '<option>' + item + '</option>';
+            });
+            return optionView;
+        }
+        
+        //update options with well names
+        var wellOptions = updateOptions(wellNames);
+        $("#wellName").html(wellOptions);
+        
+        //find well object by well name
+        function findWell(well) {
+            return well.well_name === selectedWell;
+        }
+        
+        //use selected well object to ouput to the page
         function updateWell() {
             var output = "";
             selectedWell = $("option:selected").val();
             var selectedWellObj = wells.find(findWell);
 
+            //build and format output
             var buildOutput = function () {
                 var keys = Object.keys(selectedWellObj);
 
@@ -20,13 +48,15 @@ $(document).ready(function () {
                     var value = selectedWellObj[key];
                     output += '<li><span class="key">' + key.toUpperCase(key).replace(/_/g, " ") + '</span> : <span class="value">' + value + '</span></li>';
                 });
+                
+                //display output
                 $(".records-container").html(output);
             }
             buildOutput();
         }
 
         $("button").click(function () {
-            if ($("option:selected").val() === 'Select Well') {
+            if ($("option:selected").val() === 'SELECT WELL') {
                 var output = "";
                 $(".records-container").html(output);
             } else {
